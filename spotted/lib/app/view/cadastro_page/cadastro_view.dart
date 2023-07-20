c4ff2712cdfa6f928ef17611c020128fdbd6a1f9
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2023-07-20 15:32:28 -0300

    fix: inputs

--- spotted/lib/app/view/cadastro_page/cadastro_view.dart
+++ spotted/lib/app/view/cadastro_page/cadastro_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import '../../repository/usuario_repository.dart';
 
 class CadastroPage extends StatefulWidget {
@@ -19,172 +18,99 @@ class _CadastroPageState extends State<CadastroPage> {
   String sobrenome = '';
   String telefone = '';
 
+  Widget _buildTextFormField(String label, Function(String) onChanged) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4.0),
+      child: TextFormField(
+        onChanged: onChanged,
+        decoration: InputDecoration(
+          labelText: label,
+          border: OutlineInputBorder(),
+          contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 12.0),
+        ),
+      ),
+    );
+  }
+
   Widget _body() {
     return SingleChildScrollView(
-      child: SizedBox(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                width: 100,
-                height: 100,
-                child: Image.asset('assets/images/logo.png'),
-              ),
-              Container(
-                height: 20,
-              ),
-              Card(
-                color: Colors.white,
-                child: Padding(
-                  padding: const EdgeInsets.only(
-                      left: 12, right: 12, top: 20, bottom: 12),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              width: 100,
+              height: 100,
+              child: Image.asset('assets/images/logo.png'),
+            ),
+            Container(
+              height: 20,
+            ),
+            Card(
+              color: Colors.white,
+              child: Padding(
+                padding: const EdgeInsets.all(12),
+                child: Expanded(
                   child: Column(
                     children: [
-                      TextField(
-                        onChanged: (text) {
-                          user = text;
-                        },
-                        obscureText: true,
-                        decoration: InputDecoration(
-                          labelText: 'User',
-                          border: OutlineInputBorder(),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      TextField(
-                        onChanged: (text) {
-                          email = text;
-                        },
-                        keyboardType: TextInputType.emailAddress,
-                        decoration: InputDecoration(
-                          labelText: 'Email',
-                          border: OutlineInputBorder(),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      TextField(
-                        onChanged: (text) {
-                          password = text;
-                        },
-                        obscureText: true,
-                        decoration: InputDecoration(
-                          labelText: 'Password',
-                          border: OutlineInputBorder(),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      TextField(
-                        onChanged: (text) {
-                          confirmPassword = text;
-                        },
-                        obscureText: true,
-                        decoration: InputDecoration(
-                          labelText: 'Confirm your Password',
-                          border: OutlineInputBorder(),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      TextField(
-                        onChanged: (text) {
-                          dataNascimento = text;
-                        },
-                        keyboardType: TextInputType.datetime,
-                        decoration: InputDecoration(
-                          labelText: 'Data de Nascimento',
-                          border: OutlineInputBorder(),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      TextField(
-                        onChanged: (text) {
-                          nome = text;
-                        },
-                        decoration: InputDecoration(
-                          labelText: 'Nome',
-                          border: OutlineInputBorder(),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      TextField(
-                        onChanged: (text) {
-                          sobrenome = text;
-                        },
-                        decoration: InputDecoration(
-                          labelText: 'Sobrenome',
-                          border: OutlineInputBorder(),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      TextField(
-                        onChanged: (text) {
-                          telefone = text;
-                        },
-                        keyboardType: TextInputType.phone,
-                        decoration: InputDecoration(
-                          labelText: 'Telefone',
-                          border: OutlineInputBorder(),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 15,
-                      ),
-                      ElevatedButton(
-                        onPressed: () {
-                          //CHAMAR API PARA CADASTRAR
-                          _cadastrarUsuario();
-                        },
-                        child: Container(
-                          width: double.infinity,
-                          child: Text('Cadastrar', textAlign: TextAlign.center),
-                        ),
-                        style: ElevatedButton.styleFrom(
-                          textStyle: TextStyle(
-                            fontSize: 15,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
+                      _buildTextFormField('User', (text) => user = text),
+                      SizedBox(height: 8),
+                      _buildTextFormField('Email', (text) => email = text),
+                      SizedBox(height: 8),
+                      _buildTextFormField('Password', (text) => password = text),
+                      SizedBox(height: 8),
+                      _buildTextFormField(
+                          'Confirm your Password', (text) => confirmPassword = text),
+                      SizedBox(height: 8),
+                      _buildTextFormField('Data de Nascimento', (text) => dataNascimento = text),
+                      SizedBox(height: 8),
+                      _buildTextFormField('Nome', (text) => nome = text),
+                      SizedBox(height: 8),
+                      _buildTextFormField('Sobrenome', (text) => sobrenome = text),
+                      SizedBox(height: 8),
+                      _buildTextFormField('Telefone', (text) => telefone = text),
                     ],
                   ),
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
+            ),
+            SizedBox(height: 15),
+            ElevatedButton(
+              onPressed: () {
+                //CHAMAR API PARA CADASTRAR
+                _cadastrarUsuario();
+              },
+              child: Container(
+                width: double.infinity,
+                child: Text('Cadastrar', textAlign: TextAlign.center),
               ),
-              GestureDetector(
-                onTap: () {
-                  Navigator.of(context).pushReplacementNamed('/');
-                },
-                child: Text(
-                  'Já tem uma conta? Logar.',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
+              style: ElevatedButton.styleFrom(
+                textStyle: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
                 ),
               ),
-            ],
-          ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.of(context).pushReplacementNamed('/');
+              },
+              child: Text(
+                'Já tem uma conta? Logar.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
