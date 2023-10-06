cbfd095c3e534c4b8a977ea8737c747a604eb5fd
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2023-10-05 22:08:13 -0300

    fix: Provider

--- spotted/lib/app/view/profile_page/my_products.dart
+++ spotted/lib/app/view/profile_page/my_products.dart
@@ -37,10 +37,10 @@ class _MyProductsPageState extends State<MyProductsPage> {
   Future<void> _loadUserData() async {
     try {
       final userProvider = Provider.of<UserProvider>(context, listen: false);
-      var response = await usuarioRepository.getUsuario(userProvider.user!.idUsuario);
-      print(response.nomeUsuario);
-      userProvider.setUser(response);
-     /*  _usuarioProvider = userProvider.user; */
+      var response =
+          await usuarioRepository.getUsuario(userProvider.user!.idUsuario);
+      print(response.listaArtefatosReponse);
+      userProvider.updateUserInfo(response);
     } catch (e) {
       print('Erro ao carregar o usuário: $e');
     }
@@ -49,178 +49,186 @@ class _MyProductsPageState extends State<MyProductsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Meus cadastros no app"),
-          leading: BackButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const HomePage()),
+      appBar: AppBar(
+        title: const Text("Meus cadastros no app"),
+        leading: BackButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const HomePage()),
+            );
+          },
+        ),
+      ),
+      body: Consumer<UserProvider>(
+        builder: (context, userProvider, _) {
+          var listaProdutos = userProvider.user?.listaArtefatosReponse;
+          if (listaProdutos == null || listaProdutos.isEmpty) {
+            return GridView.builder(
+              padding: const EdgeInsets.all(20),
+              gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                maxCrossAxisExtent: 200,
+                childAspectRatio: 2 / 3,
+                crossAxisSpacing: 20,
+                mainAxisSpacing: 20,
+              ),
+              itemCount: 0,
+              itemBuilder: (BuildContext ctx, index) {
+                return GridTile(
+                  child: Container(),
+                );
+              },
+            );
+          }
+          return GridView.builder(
+            padding: const EdgeInsets.all(20),
+            gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 200,
+              childAspectRatio: 2 / 3,
+              crossAxisSpacing: 20,
+              mainAxisSpacing: 20,
+            ),
+            itemCount: listaProdutos.length,
+            itemBuilder: (BuildContext ctx, index) {
+              var produto = userProvider.user?.listaArtefatosReponse?[index];
+              String tipoArtefato = userProvider
+                  .user?.listaArtefatosReponse?[index]["tipoArtefato"];
+              return GridTile(
+                key: ValueKey(produto),
+                footer: GridTileBar(
+                  backgroundColor: const Color.fromARGB(137, 107, 98, 98),
+                  title: Text(
+                    userProvider.user?.listaArtefatosReponse?[index]
+                        ["tituloArtefato"],
+                    style: const TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  subtitle: Text(
+                    userProvider.user?.listaArtefatosReponse?[index]
+                        ["descricaoArtefato"],
+                    style: const TextStyle(
+                      fontSize: 13,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  trailing: PopupMenuButton(
+                    itemBuilder: (BuildContext context) => [
+                      PopupMenuItem(
+                        child: OutlinedButton(
+                          child: const Text('Editar'),
+                          onPressed: () {
+                            switch (tipoArtefato) {
+                              case "EMPREGO":
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) =>
+                                        EmpregoEditarView(produto),
+                                  ),
+                                );
+                                break;
+                              case "ALIMENTO":
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) =>
+                                        AlimentoEditarView(produto),
+                                  ),
+                                );
+                                break;
+                              case "EVENTO":
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) =>
+                                        EventoEditarView(produto),
+                                  ),
+                                );
+                                break;
+                              case "OBJETO":
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) =>
+                                        ObjetoEditarView(produto),
+                                  ),
+                                );
+                                break;
+                              case "MORADIA":
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) =>
+                                        MoradiaEditarView(produto),
+                                  ),
+                                );
+                                break;
+                              case "TRANSPORTE":
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) =>
+                                        TransporteEditarView(produto),
+                                  ),
+                                );
+                                break;
+                              default:
+                                print("Ocorreu um erro ao editar o produto.");
+                                print(tipoArtefato);
+                            }
+                          },
+                        ),
+                      ),
+                      PopupMenuItem(
+                        child: OutlinedButton(
+                          child: const Text('Deletar'),
+                          onPressed: () {
+                            switch (tipoArtefato) {
+                              case "EMPREGO":
+                                EmpregoDeletarView(produto).inativar(context);
+                                break;
+                              case "ALIMENTO":
+                                AlimentoDeletarView(produto).inativar(context);
+                                break;
+                              case "EVENTO":
+                                EventoDeletarView(produto).inativar(context);
+                                break;
+                              case "OBJETO":
+                                ObjetoDeletarView(produto).inativar(context);
+                                break;
+                              case "MORADIA":
+                                MoradiaDeletarView(produto).inativar(context);
+                                break;
+                              case "TRANSPORTE":
+                                TransporteDeletarView(produto)
+                                    .inativar(context);
+                                break;
+                              default:
+                                print("Ocorreu um erro ao deletar o produto.");
+                            }
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                child: Column(
+                  children: [
+                    Expanded(
+                      child: Image.network(
+                        produto["listaImagens"][0]["url"],
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ],
+                ),
               );
             },
-          ),
-        ),
-        body: _listarMeusProdutos());
-  }
-
-  GridView _listarMeusProdutos() {
-    var listaProdutos = _usuarioProvider?.listaArtefatosReponse;
-    if (listaProdutos == null || listaProdutos.isEmpty) {
-      return GridView.builder(
-        padding: const EdgeInsets.all(20),
-        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          childAspectRatio: 2 / 3,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20,
-        ),
-        itemCount: 0,
-        itemBuilder: (BuildContext ctx, index) {
-          return GridTile(
-            child: Container(),
           );
         },
-      );
-    }
-    return GridView.builder(
-      padding: const EdgeInsets.all(20),
-      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 2 / 3,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
       ),
-      itemCount: listaProdutos.length,
-      itemBuilder: (BuildContext ctx, index) {
-        var produto = _usuarioProvider?.listaArtefatosReponse?[index];
-        String tipoArtefato =
-            _usuarioProvider?.listaArtefatosReponse?[index]["tipoArtefato"];
-        return GridTile(
-          key: ValueKey(produto),
-          footer: GridTileBar(
-            backgroundColor: const Color.fromARGB(137, 107, 98, 98),
-            title: Text(
-              _usuarioProvider?.listaArtefatosReponse?[index]["tituloArtefato"],
-              style: const TextStyle(
-                fontSize: 15,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            subtitle: Text(
-              _usuarioProvider?.listaArtefatosReponse?[index]
-                  ["descricaoArtefato"],
-              style: const TextStyle(
-                fontSize: 13,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            trailing: PopupMenuButton(
-              itemBuilder: (BuildContext context) => [
-                PopupMenuItem(
-                  child: OutlinedButton(
-                    child: const Text('Editar'),
-                    onPressed: () {
-                      switch (tipoArtefato) {
-                        case "EMPREGO":
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => EmpregoEditarView(produto),
-                            ),
-                          );
-                          break;
-                        case "ALIMENTO":
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => AlimentoEditarView(produto),
-                            ),
-                          );
-                          break;
-                        case "EVENTO":
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => EventoEditarView(produto),
-                            ),
-                          );
-                          break;
-                        case "OBJETO":
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => ObjetoEditarView(produto),
-                            ),
-                          );
-                          break;
-                        case "MORADIA":
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => MoradiaEditarView(produto),
-                            ),
-                          );
-                          break;
-                        case "TRANSPORTE":
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) =>
-                                  TransporteEditarView(produto),
-                            ),
-                          );
-                          break;
-                        default:
-                          print("Ocorreu um erro ao editar o produto.");
-                          print(tipoArtefato);
-                      }
-                    },
-                  ),
-                ),
-                PopupMenuItem(
-                  child: OutlinedButton(
-                    child: const Text('Deletar'),
-                    onPressed: () {
-                      switch (tipoArtefato) {
-                        case "EMPREGO":
-                          EmpregoDeletarView(produto).inativar(context);
-                          break;
-                        case "ALIMENTO":
-                          AlimentoDeletarView(produto).inativar(context);
-                          break;
-                        case "EVENTO":
-                          EventoDeletarView(produto).inativar(context);
-                          break;
-                        case "OBJETO":
-                          ObjetoDeletarView(produto).inativar(context);
-                          break;
-                        case "MORADIA":
-                          MoradiaDeletarView(produto).inativar(context);
-                          break;
-                        case "TRANSPORTE":
-                          TransporteDeletarView(produto).inativar(context);
-                          break;
-                        default:
-                          print("Ocorreu um erro ao deletar o produto.");
-                      }
-                    },
-                  ),
-                ),
-              ],
-            ),
-          ),
-          child: Column(
-            children: [
-              Expanded(
-                child: Image.network(
-                  produto["listaImagens"][0]["url"],
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ],
-          ),
-        );
-      },
     );
   }
 }
